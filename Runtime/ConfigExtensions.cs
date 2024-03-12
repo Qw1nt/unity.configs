3dbff321522b4e47680486d6a49fd6226b101a99
Qw1nt <pasharudnykh3@mail.ru>
2024-03-12 15:25:34 +0500

    [*] API

--- Runtime/ConfigExtensions.cs
+++ Runtime/ConfigExtensions.cs
@@ -1,16 +1,74 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+#if CONFIG_CONTENT_CONTROLLER_INTEGRATION
 using Qw1nt.Runtime.AddressablesContentController.Common;
 using Qw1nt.Runtime.AddressablesContentController.Core;
+#endif
 
 namespace Qw1nt.Runtime.Config
 {
     public static class ConfigExtensions
     {
-        public static async UniTask<ContentOperation<TConfig>> LoadAsync<TConfig, TLoader>(this ConfigBase<TConfig, TLoader> config)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static LoadConfigOperation<TConfig, TLoader> Load<TConfig, TLoader>(
+            this ConfigBase<TConfig, TLoader> config)
             where TConfig : ConfigBase<TConfig, TLoader>
             where TLoader : struct, IConfigLoader<TConfig, TLoader>
         {
-            return await ContentController.Default.LoadAsync<TConfig>(new TLoader().Key);
+            return new LoadConfigOperation<TConfig, TLoader>(new TLoader());
         }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static TConfig FromResources<TConfig, TLoader>(
+            this LoadConfigOperation<TConfig, TLoader> operation)
+            where TConfig : ConfigBase<TConfig, TLoader>
+            where TLoader : struct, IConfigLoader<TConfig, TLoader>
+        {
+            return Resources.Load<TConfig>(operation.Loader.Key);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static async UniTask<TConfig> FromResourcesAsync<TConfig, TLoader>(
+            this LoadConfigOperation<TConfig, TLoader> operation)
+            where TConfig : ConfigBase<TConfig, TLoader>
+            where TLoader : struct, IConfigLoader<TConfig, TLoader>
+        {
+            return (TConfig) await Resources.LoadAsync<TConfig>(operation.Loader.Key);
+        }
+
+#if !UNITY_WEBGL
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static TConfig FromAddressables<TConfig, TLoader>(
+            this LoadConfigOperation<TConfig, TLoader> operation)
+            where TConfig : ConfigBase<TConfig, TLoader>
+            where TLoader : struct, IConfigLoader<TConfig, TLoader>
+        {
+            return Addressables.LoadAssetAsync<TConfig>(operation.Loader.Key).WaitForCompletion();
+        }
+#endif
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static async UniTask<TConfig> FromAddressablesAsync<TConfig, TLoader>(
+            this LoadConfigOperation<TConfig, TLoader> operation)
+            where TConfig : ConfigBase<TConfig, TLoader>
+            where TLoader : struct, IConfigLoader<TConfig, TLoader>
+        {
+            return await Addressables.LoadAssetAsync<TConfig>(operation.Loader.Key);
+        }
+
+#if CONFIG_CONTENT_CONTROLLER_INTEGRATION
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static async UniTask<ContentOperation<TConfig>> FromContentControllerAsync<TConfig, TLoader>(
+            this LoadConfigOperation<TConfig, TLoader> operation)
+            where TConfig : ConfigBase<TConfig, TLoader>
+            where TLoader : struct, IConfigLoader<TConfig, TLoader>
+        {
+            return await ContentController.Default.LoadAsync<TConfig>(operation.Loader.Key);
+        }
+#endif
     }
 }
---
+++ Runtime/LoadConfigOperation.cs
@@ -0,0 +1,19 @@
+﻿namespace Qw1nt.Runtime.Config
+{
+    public struct LoadConfigOperation<TConfig, TLoader>
+        where TConfig : ConfigBase<TConfig, TLoader>
+        where TLoader : struct, IConfigLoader<TConfig, TLoader>
+    {
+        public LoadConfigOperation(TLoader loader)
+        {
+            Loader = loader;
+        }
+
+        public readonly TLoader Loader;
+
+        public static implicit operator string(LoadConfigOperation<TConfig, TLoader> operation)
+        {
+            return operation.Loader.Key;
+        }
+    }
+}
